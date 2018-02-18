366cc4d379494f548072d955d4c606e82c92b0ba
Sören Tempel <soeren+git@soeren-tempel.net>
2018-02-18 19:47:39 +0100

    compat/POSIX: refactor DEBUG macro

--- compat/include/POSIX/debug.h
+++ compat/include/POSIX/debug.h
@@ -3,12 +3,6 @@
 
 #include <stdio.h>
 
-#define DEBUG_PRINT(...) printf(__VA_ARGS__)
-
-#if ENABLE_DEBUG
-  #define DEBUG(...) DEBUG_PRINT(__VA_ARGS__)
-#else
-  #define DEBUG(...)
-#endif
+#define DEBUG(...) if (ENABLE_DEBUG) printf(__VA_ARGS__)
 
 #endif
